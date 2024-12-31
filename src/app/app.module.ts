987ffab3fa2710c60d7de1c68ffd282e123ec93a
Abu_Programmiy <abdukholiq23@gmail.com>
2024-12-31 07:53:12 +0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { GenderSelectionComponent } from './components/secondary/gender-selection/gender-selection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    GenderSelectionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/secondary/gender-selection/gender-selection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gender-selection',
+  templateUrl: './gender-selection.component.html',
+  styleUrl: './gender-selection.component.scss'
+})
+export class GenderSelectionComponent {
+
+}
